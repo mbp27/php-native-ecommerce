53f8f995f6cbb4a01873f9d4dc55c698d067f059
prasetyobagus27@gmail.com <mbp27>
2019-03-05 07:28:21 +0700

    added

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Accessories Shop</title>
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+	<link href="assets/css/carousel.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">	
+	<script src="assets/js/jquery-3.3.1.js"></script>
+</head>
+
+<body>
+	<header>
+		<div class="navbar-wrapper">
+			<div class="container">
+				<div class="container-fluid">
+					<nav class="navbar navbar-inverse navbar-static-top">
+						<div class="container">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="#">Project name</a>
+							</div>
+							<div id="navbar" class="navbar-collapse collapse">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="#">Home</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Category <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+										</ul>
+									</li>
+								</ul>
+							    <ul class="nav navbar-nav navbar-right">
+							      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+							      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+							    </ul>
+							</div>
+						</div>
+					</nav>
+				</div>
+
+			</div>
+		</div>
+	</header>
+
+	<main>
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img class="first-slide" src="assets/img/banner1.jpg" alt="First slide">
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>Discount Up to 70%</h1>
+							<p>.</p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Shop Now</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<img class="second-slide" src="assets/img/banner2.jpg" alt="Second slide">
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>Another example headline.</h1>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Shop Now</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<img class="third-slide" src="assets/img/banner3.jpg" alt="Third slide">
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>One more for good measure.</h1>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Shop Now</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+		<div class="container marketing">
+			<div class="row">
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-lg-3" style="text-align:center;">
+					<img class="featurette-image img-responsive center-block" src="assets/img/iphonex.png" alt="Generic placeholder image" width="140" height="140">
+					<h2>Heading</h2>
+					<p>.........</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+			</div>
+			<hr class="featurette-divider">
+			<div class="row featurette">
+				<div class="col-md-7">
+					<h2 class="featurette-heading">Hot Product!</h2>
+					<p class="lead">This is hot product from Accessories Shop. Check Now!</p>
+				</div>
+				<div class="col-md-5">
+					<img src="assets/img/iphonex.png" class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+				</div>
+			</div>
+			<hr class="featurette-divider">
+			<footer>
+				<p class="pull-right"><a href="#">Back to top</a></p>
+				<p>&copy; Muhamad Bagus Prasetyo 2019. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+			</footer>
+		</div>
+	</main>
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/vendor/holder.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/app.js"></script>
+</body>
+
+</html>
